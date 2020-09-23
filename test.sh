72e8355170043f1d10e3273d42960e976ea933c9
Olav Fosse <fosseolav@gmail.com>
2020-09-23 12:20:14 +0200

    Refactor test suite to use test helpers

--- test.sh
+++ test.sh
@@ -20,45 +20,64 @@ fail() {
 	fi
 }
 
+test_exact () {
+	assertion="$1"
+	command="$2"
+	expected_output="$3"
+	actual_output="$($command 2>&1)"
+
+	if [ ! "$actual_output" = "$expected_output" ]; then
+		echo '---ASSERTION---'
+		echo "$assertion"
+		echo '---COMMAND---'
+		echo "$command"
+		echo '---EXPECTED OUTPUT---'
+		echo "$expected_output"
+		echo '---ACTUAL OUTPUT---'
+		echo "$actual_output"
+		fail
+	fi
+}
+
+test_pattern () {
+	assertion="$1"
+	command="$2"
+	expected_output_pattern="$3"
+	actual_output="$($command 2>&1)"
+
+	case "$actual_output" in
+		$expected_output_pattern)
+			;;
+		*)
+			echo '---ASSERTION---'
+			echo "$assertion"
+			echo '---COMMAND---'
+			echo "$command"
+			echo '---EXPECTED OUTPUT PATTERN---'
+			echo "$expected_output_pattern"
+			echo '---ACTUAL OUTPUT---'
+			echo "$actual_output"
+			fail
+	esac
+}
+
 # ---Tests---
 # Test 1
 assertion='Error message is printed when too few arguments are passed'
 command='./0x0.sh file'
 expected_output="$USAGE"
-actual_output="$($command 2>&1)"
 
-if [ ! "$actual_output" = "$expected_output" ]; then
-	echo '---ASSERTION---'
-	echo "$assertion"
-	echo '---COMMAND---'
-	echo "$command"
-	echo '---EXPECTED OUTPUT---'
-	echo "$expected_output"
-	echo '---ACTUAL OUTPUT---'
-	echo "$actual_output"
-	fail
-fi
+test_exact "$assertion" "$command" "$expected_output"
 
 # Test 2
 assertion='Error message is printed when too many arguments are passed'
 command='./0x0.sh file file1 file2'
 expected_output="$USAGE"
-actual_output="$($command 2>&1)"
 
-if [ ! "$actual_output" = "$expected_output" ]; then
-	echo '---ASSERTION---'
-	echo "$assertion"
-	echo '---COMMAND---'
-	echo "$command"
-	echo '---EXPECTED OUTPUT---'
-	echo "$expected_output"
-	echo '---ACTUAL OUTPUT---'
-	echo "$actual_output"
-	fail
-fi
+test_exact "$assertion" "$command" "$expected_output"
 
 # Test 3
-# HACK I was not able to escape the command properly, so the literal command is repeated in two lines
+# HACK I was not able to escape the command properly, so the test helpers cannot be used
 assertion='File is uploaded from stdin'
 expected_output_pattern='http://0x0.st/*.txt'
 actual_output="$(echo "I want to share this with my friends on irc" | ./0x0.sh file - 2>&1)"
@@ -81,117 +100,46 @@ esac
 # Test 4
 assertion='File is uploaded from disk'
 file_name='/tmp/0x0.sh.temp'
-echo '#!/bin/sh' >> "$file_name"
-echo 'echo hello, world' >> "$file_name"
 command="./0x0.sh file $file_name"
 expected_output_pattern='http://0x0.st/*.temp'
-actual_output="$($command 2>&1)"
 
-case "$actual_output" in
-	$expected_output_pattern)
-		;;
-	*)
-		echo '---ASSERTION---'
-		echo "$assertion"
-		echo '---COMMAND---'
-		echo "$command"
-		echo '---EXPECTED OUTPUT PATTERN---'
-		echo "$expected_output_pattern"
-		echo '---ACTUAL OUTPUT---'
-		echo "$actual_output"
-		fail
-esac
+echo '#!/bin/sh' >> "$file_name"
+echo 'echo hello, world' >> "$file_name"
+
+test_pattern "$assertion" "$command" "$expected_output_pattern"
 
 # Test 5
 assertion='File is uploaded from URL'
-url='https://fossegr.im/'
-command="./0x0.sh url $url"
+command="./0x0.sh url https://fossegr.im"
 expected_output_pattern='https://0x0.st/*.html'
-actual_output="$($command 2>&1)"
 
-case "$actual_output" in
-	$expected_output_pattern)
-		;;
-	*)
-		echo '---ASSERTION---'
-		echo "$assertion"
-		echo '---COMMAND---'
-		echo "$command"
-		echo '---EXPECTED OUTPUT PATTERN---'
-		echo "$expected_output_pattern"
-		echo '---ACTUAL OUTPUT---'
-		echo "$actual_output"
-		fail
-esac
+test_pattern "$assertion" "$command" "$expected_output_pattern"
 
 # Test 6
 assertion='URL is shortened'
-url='https://fossegr.im/'
-command="./0x0.sh shorten $url"
+command="./0x0.sh shorten https://fossegr.im/"
 expected_output_pattern='https://0x0.st/*'
-actual_output="$($command 2>&1)"
 
-case "$actual_output" in
-	$expected_output_pattern)
-		;;
-	*)
-		echo '---ASSERTION---'
-		echo "$assertion"
-		echo '---COMMAND---'
-		echo "$command"
-		echo '---EXPECTED OUTPUT PATTERN---'
-		echo "$expected_output_pattern"
-		echo '---ACTUAL OUTPUT---'
-		echo "$actual_output"
-		fail
-esac
+test_pattern "$assertion" "$command" "$expected_output_pattern"
 
 # Test 7
 assertion='Error message is printed when attempt to upload non-existant file'
 file="/tmp/non-existant-file"
 command="./0x0.sh file $file"
 expected_output="error: $file does not exist"
-actual_output="$($command 2>&1)"
 
-case "$actual_output" in
-	$expected_output)
-		;;
-	*)
-		echo '---ASSERTION---'
-		echo "$assertion"
-		echo '---COMMAND---'
-		echo "$command"
-		echo '---EXPECTED OUTPUT---'
-		echo "$expected_output"
-		echo '---ACTUAL OUTPUT---'
-		echo "$actual_output"
-		fail
-esac
+test_exact "$assertion" "$command" "$expected_output"
 
 # Test 8
 assertion='Error message is printed when attempt to upload directory'
 directory="/tmp/"
 command="./0x0.sh file $directory"
 expected_output="error: $directory is a directory"
-actual_output="$($command 2>&1)"
 
-case "$actual_output" in
-	$expected_output)
-		;;
-	*)
-		echo '---ASSERTION---'
-		echo "$assertion"
-		echo '---COMMAND---'
-		echo "$command"
-		echo '---EXPECTED OUTPUT---'
-		echo "$expected_output"
-		echo '---ACTUAL OUTPUT---'
-		echo "$actual_output"
-		fail
-esac
+test_exact "$assertion" "$command" "$expected_output"
 
 # Test 9
-# HACK I was not able to escape the command properly, so the literal command is repeated in two lines
+# HACK I was not able to escape the command properly, so the test helpers cannot be used
 assertion='Error message is printed when curl not in PATH'
 expected_output='error: curl: not found'
 actual_output="$(SIMULATE_CURL_NOT_IN_PATH=true ./0x0.sh 2>&1)"
